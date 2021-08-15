8835670bf2dc8b2271226a65d1a04cab5099b314
panoramine <camila.c.a.salles@gmail.com>
2021-08-15 11:58:51 -0700

    Add more conditionals examples

--- conditionals.js
+++ conditionals.js
@@ -1,4 +1,3 @@
-/* 
 const raining = true;
 const cold = true;
 
@@ -12,7 +11,8 @@ if (cold) {
 
 console.log("Now you're ready to go outside!");
 
-*/
+
+
 
 const temperature = 11;
 
@@ -25,3 +25,24 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+
+
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea...");
+} 
+
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
