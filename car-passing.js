0be9580a9b02c50349758dc85fa532a7b38e9555
panoramine <camila.c.a.salles@gmail.com>
2021-08-20 07:23:14 -0700

    function that creates new object and add it to an array

---
+++ car-passing.js
@@ -0,0 +1,29 @@
+const cars = [
+  {
+    time: 1568329654807,
+    speed: 40,
+  },
+  {
+    time: 1568329821632,
+    speed: 42,
+  },
+  {
+    time: 1568331115463,
+    speed: 35
+  }
+]
+
+
+const carPassing = function (cars, speed) {
+  let tTime = Date.now()
+  let car = {
+    time: tTime,
+    'speed': speed
+  }
+  cars.push(car);
+  return cars;
+}
+
+const speed = 38;
+
+console.log(carPassing(cars, speed));
