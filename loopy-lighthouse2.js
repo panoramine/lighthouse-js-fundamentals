bfca59f3faa46906b4ceadeaae76eacef5d55b6c
panoramine <camila.c.a.salles@gmail.com>
2021-08-30 22:27:19 -0700

    in a given interval function return string or number

---
+++ loopy-lighthouse2.js
@@ -0,0 +1,15 @@
+const loopyLighthouse = function(range, multiples, words) {
+  for (let x = range[0]; x < (range[1] + 1); x++) {
+    if (x % multiples[0] === 0 && x % multiples[1] !== 0) {
+      console.log(words[0]);
+    } else if (x % multiples[0] === 0 && x % multiples[1] === 0) {
+      console.log(words[0] + words[1]);
+    } else if (x % multiples[0] !== 0 && x % multiples[1] === 0) {
+      console.log(words[1]);
+    } else {
+      console.log(x);
+    }
+  }
+}
+
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
